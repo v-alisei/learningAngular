754ba59674e33d3bf0c9212fc75aa9f2f8a4a307
v-alisei <valisei@pm.me>
2021-11-29 14:36:54 +0300

    added button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  test():void{
+    console.log("hiiiiiiiiiii")
+  }
   title = 'learningAngular';
 }
