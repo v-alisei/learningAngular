ffcf99249da5faee21606078a106dd64346146cc
v-alisei <valisei@pm.me>
2021-11-29 13:24:18 +0300

    tested angular syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+function test():void{
+  console.log('hiiiiii')
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(){ 
+    test()
+  }
   title = 'learningAngular';
+  
 }
