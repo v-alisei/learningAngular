372bc9f0edcd3ef87e3c878db724a6cfd69d7b8f
v-alisei <valisei@pm.me>
2021-12-27 16:35:48 +0300

    made behaviourSubject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
-  
+export class AppComponent implements OnInit {
+  sbj:BehaviorSubject<number>
+  constructor(){ 
+    this.sbj = new BehaviorSubject<number>(0);
+    this.sbj.subscribe(this.log)
+  }
+  ngOnInit() {
+    setInterval(()=>{this.sbj.next(Math.random()*1000)}, 200)
+  }
+  log(n:number){
+    if(n>300 && n<700) console.log(n)
+  }
 }
